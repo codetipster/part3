017e33b82229669ea25d640b20b44df9e10a79c7
codetipster <talk2samuelnzekwe@gmail.com>
2024-12-12 11:18:08 +0100

    linting update

--- index.js
+++ index.js
@@ -1,92 +1,68 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
-
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.static('dist')) //serve static files from dist folder
-
-//configuration to show body of post request when using morgan for logging requests
-morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-  {
-    'id': '1',
-    'name': 'Arto Hellas',
-    'number': '040-123456'
-  },
-  {
-    'id': '2',
-    'name': 'Ada Lovelace',
-    'number': '39-44-5323523'
-  },
-  {
-    'id': '3',
-    'name': 'Dan Abramov',
-    'number': '12-43-234345'
-  },
-  {
-    'id': '4',
-    'name': 'Mary Poppendieck',
-    'number': '39-23-6423122'
-  }
-]
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 
-
-
-app.get('/info', (req, res) => {
-  const date = new Date()
-  const personsLength = persons.length
-  res.send('<div>Phonebook has info for ' + personsLength + ' people</div><div>' + date + '</div>')
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
+// const express = require('express')
+// const morgan = require('morgan')
+// const cors = require('cors')
+// const app = express()
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
+// const app = require('./app')
+// const config = require('./utils/config')
+// const logger = require('./utils/logger')
 
-app.get('/api/persons/:id', (req, res) => {
-  const person = persons.find(p => p.id === req.params.id)
-  if(person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+// app.use(express.json())
+// app.use(morgan('dev'))
+// app.use(cors())
+// app.use(express.static('dist')) //serve static files from dist folder
 
-})
+// //configuration to show body of post request when using morgan for logging requests
+// morgan.token('body', (req) => JSON.stringify(req.body))
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
 
-  res.status(204).end()
-})
+// app.get('/api/persons/:id', (req, res) => {
+//   const person = persons.find(p => p.id === req.params.id)
+//   if(person) {
+//     res.json(person)
+//   } else {
+//     res.status(404).end()
+//   }
 
-app.post('/api/persons', (req, res) => {
-  const person = req.body
-  person.id = Math.floor(Math.random() * 1000000)
+// })
 
-  if(!person.name || !person.number) {
-    return res.status(400).json({ error: 'name or number missing' })
-  }
+// app.delete('/api/persons/:id', (req, res) => {
+//   const id = req.params.id
+//   persons = persons.filter(person => person.id !== id)
 
-  const existingPerson = persons.find(p => p.name === person.name)
+//   res.status(204).end()
+// })
 
-  if(existingPerson) {
-    return res.status(400).json({ error: 'name must be unique' })
-  }
+// app.post('/api/persons', (req, res) => {
+//   const person = req.body
+//   person.id = Math.floor(Math.random() * 1000000)
 
-  persons = persons.concat(person)
+//   if(!person.name || !person.number) {
+//     return res.status(400).json({ error: 'name or number missing' })
+//   }
 
-  res.json(person)
+//   const existingPerson = persons.find(p => p.name === person.name)
 
-})
+//   if(existingPerson) {
+//     return res.status(400).json({ error: 'name must be unique' })
+//   }
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+//   persons = persons.concat(person)
+
+//   res.json(person)
+
+// })
+
+// app.listen(config.PORT, () => {
+//   logger.info(`Server running on port ${config.PORT}`)
+// })
---
+++ models/person.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  number: {
+    type: Number,
+    required: true,
+    minlength: 11
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
